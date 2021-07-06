95ba52082a21f7a23b1ef16849aa209acdacf703
Alexander Acker <alexander.acker@tu-berlin.de>
2021-07-06 16:28:23 +0200

    Implemented check-log-quality as github action.

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# ========================================
+# =               Warning!               =
+# ========================================
+# This is GitHub Action docker-based image.
+# It is not intended for local development!
+#
+# The original image is available on Dockerhub,
+# use it instead:
+# https://hub.docker.com/r/logsightaihub/check-log-quality
+
+FROM logsightaihub/check-log-quality:test
+
+# Metadata:
+LABEL maintainer="logsight.ai"
+LABEL vendor="logsight.ai"
+
+# Entrypoint:
+COPY ./entrypoint.sh /
+RUN chmod +x /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Diagnostic output:
+echo "Using options: $INPUT_OPTIONS"
+echo '============================='
+echo
+
+# Runs misspell-fixes:
+output=$(/check-log-quality/check-log-quality $INPUT_OPTIONS)
+status="$?"
+
+# Sets the output variable for GitHub Action API:
+# See: https://help.github.com/en/articles/development-tools-for-github-action
+echo "::set-output name=output::$output"
+echo '================================='
+echo
+
+# Fail the build in case status code is not 0:
+if [ "$status" -ne 0 ]; then
+  echo 'Failing with output:'
+  echo "$output"
+  echo "Process failed with the status code: $status"
+  exit "$status"
+fi
